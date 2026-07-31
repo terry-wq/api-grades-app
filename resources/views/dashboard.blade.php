63d4557eaf91b176a978bc3cc0c3292e2135dddf
tlapanco <99222342+tlapanco@users.noreply.github.com>
2026-07-31 13:28:06 -0600

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,8 +5,6 @@
 
     @if(($activeTab ?? 'podium') === 'podium')
         @include('partials.podium')
-    @elseif(($activeTab ?? 'podium') === 'reveal')
-        @include('partials.reveal')
     @elseif(($activeTab ?? 'podium') === 'list')
         @include('partials.student-list')
     @elseif(($activeTab ?? 'podium') === 'stats')
