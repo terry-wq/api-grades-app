5672daf4ce63f7b8e6c8891fa2177dc8cf90a317
tlapanco <99222342+tlapanco@users.noreply.github.com>
2026-08-03 15:50:07 -0600

    Create cors.php

---
+++ config/cors.php
@@ -0,0 +1,21 @@
+return [
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://students-view-api.vercel.app',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+
+];
